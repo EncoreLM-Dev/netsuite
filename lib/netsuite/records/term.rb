0463c30d53f76bfddbd655c31a5e750dffcea842
nanosplit <jacob@mpulsedesign.com>
2024-09-04 22:00:04 -0500

    Add new fields to Term record
    Include recurrence_frequency and installment fields in Term record. This enhances the object with new attributes needed for upcoming features.

--- lib/netsuite/records/term.rb
+++ lib/netsuite/records/term.rb
@@ -11,7 +11,7 @@ module NetSuite
 
       fields :due_next_month_if_within_days, :name, :date_driven, :days_until_expiry, :days_until_net_due,
         :day_discount_expires, :day_of_month_net_due, :discount_percent, :discount_percent_date_driven, :is_inactive,
-        :preferred
+        :preferred, :recurrence_frequency, :installment, :days_until_net_due, :day_discount_expires
 
       attr_reader :internal_id
       attr_accessor :external_id
